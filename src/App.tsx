cfd314be400c81c2f95fc1e62d11934e185d407b
Marco Vidali <marcovidali1@skiff.com>
2024-04-23 14:41:22 +0200

    feat(app): Implement lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useSetTheme } from "./features/theme/useSetTheme";
 import { Toaster } from "./ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Suspense, lazy } from "react";
 
 import AppLayout from "./ui/AppLayout";
-import Join from "./pages/Join";
 import UserProtectedRoute from "./features/auth/UserProtectedRoute";
+import FullPageSpinner from "./ui/FullPageSpinner";
 
 const queryClient = new QueryClient();
+
+const Join = lazy(() => import("./pages/Join"));
 
 const App = () => {
     useSetTheme();
@@ -17,21 +20,23 @@ const App = () => {
             <Toaster />
 
             <QueryClientProvider client={queryClient}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route element={<AppLayout />}>
-                            <Route
-                                index
-                                element={
-                                    <UserProtectedRoute>
-                                        <h1>home</h1>
-                                    </UserProtectedRoute>
-                                }
-                            />
-                            <Route path="/join" element={<Join />} />
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
+                <Suspense fallback={<FullPageSpinner />}>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route element={<AppLayout />}>
+                                <Route
+                                    index
+                                    element={
+                                        <UserProtectedRoute>
+                                            <h1>home</h1>
+                                        </UserProtectedRoute>
+                                    }
+                                />
+                                <Route path="/join" element={<Join />} />
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                </Suspense>
             </QueryClientProvider>
         </>
     );
