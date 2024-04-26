97928dbf2d8d0997b606e69d29260db2627123e5
Marco Vidali <marcovidali1@skiff.com>
2024-04-26 15:30:55 +0200

    feat(app): Add tooltip to sidebar icons

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,37 +1,69 @@
 import { NavLink } from "react-router-dom";
 import { Button } from "./button";
 import { IoAdd, IoAddOutline, IoHome, IoHomeOutline } from "react-icons/io5";
+import { Tooltip, TooltipProvider } from "./tooltip";
+import { TooltipContent, TooltipTrigger } from "./tooltip";
 
 const Navbar = () => {
     return (
-        <div className="flex justify-center gap-2 border-t p-4 sm:flex-col sm:justify-start sm:border-r">
-            <NavLink to="/">
-                {({ isActive }) =>
-                    isActive ? (
-                        <Button variant="ghost">
-                            <IoHome />
-                        </Button>
-                    ) : (
-                        <Button variant="ghost">
-                            <IoHomeOutline />
-                        </Button>
-                    )
-                }
-            </NavLink>
-            <NavLink to="/create">
-                {({ isActive }) =>
-                    isActive ? (
-                        <Button variant="ghost">
-                            <IoAdd />
-                        </Button>
-                    ) : (
-                        <Button variant="ghost">
-                            <IoAddOutline />
-                        </Button>
-                    )
-                }
-            </NavLink>
-        </div>
+        <TooltipProvider>
+            <div className="flex justify-center gap-2 border-t p-4 sm:flex-col sm:justify-start sm:border-r">
+                <NavLink to="/">
+                    {({ isActive }) =>
+                        isActive ? (
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button variant="ghost">
+                                        <IoHome />
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>home</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        ) : (
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button variant="ghost">
+                                        <IoHomeOutline />
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>home</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        )
+                    }
+                </NavLink>
+                <NavLink to="/create">
+                    {({ isActive }) =>
+                        isActive ? (
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button variant="ghost">
+                                        <IoAdd />
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>create</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        ) : (
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button variant="ghost">
+                                        <IoAddOutline />
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>create</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        )
+                    }
+                </NavLink>
+            </div>
+        </TooltipProvider>
     );
 };
 
