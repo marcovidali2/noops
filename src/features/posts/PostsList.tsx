3b752b09f025ae5a73aae9c877e20698834b76f0
Marco Vidali <marcovidali1@skiff.com>
2024-05-03 16:12:05 +0200

    feat(app): Add no posts alert

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,4 +1,6 @@
 import { usePosts } from "./usePosts";
+import { Alert, AlertDescription, AlertTitle } from "@/ui/alert";
+import { IoInformationCircleOutline } from "react-icons/io5";
 
 import PostItem from "./PostItem";
 
@@ -9,7 +11,15 @@ const PostsList = () => {
         <div className="space-y-4">
             <h1>posts</h1>
 
-            {posts?.length === 0 && <p>there are currently no posts</p>}
+            {posts?.length === 0 && (
+                <Alert>
+                    <IoInformationCircleOutline className="h-4 w-4" />
+                    <AlertTitle>there are no posts</AlertTitle>
+                    <AlertDescription>
+                        wait for someone to create one or create one yourself
+                    </AlertDescription>
+                </Alert>
+            )}
 
             <div className="flex flex-col gap-4">
                 {posts?.map((post) => <PostItem key={post.id} post={post} />)}
